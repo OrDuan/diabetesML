4e60a266e4c65e64774f67b8042ad50f5da79eda
or <orduani@gmail.com>
2016-08-13 21:03:58 +0300

    add k fold method

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from parser import parse_all
+from sklearn.cross_validation import KFold
 
 
 class DataHandler(object):
@@ -12,6 +13,7 @@ class DataHandler(object):
         self.activities = []
         self.dates = []
         self.glucose = []
+        self.k_fold = []
 
     def parse_data(self):
         """
@@ -27,6 +29,12 @@ class DataHandler(object):
                 self.activities.append(data['activity'])
                 self.glucose.append(data['glucose'])
 
+    def load_k_fold(self, n_folds):
+        """
+        Load the test cases into K folds
+        """
+        self.k_fold = KFold(len(self.data_list), n_folds=n_folds)
+
     def plot(self):
         """
         Matplotlib it!
@@ -35,8 +43,8 @@ class DataHandler(object):
         plt.plot(self.dates, self.activities, 'ro')
         plt.show()
 
-
 if __name__ == '__main__':
     dh = DataHandler()
     dh.parse_data()
+    dh.load_k_fold(5)
     dh.plot()

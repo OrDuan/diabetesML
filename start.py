bae6480a8eb5d235f154a3025f33e18b8e626cd8
or <orduani@gmail.com>
2016-08-14 19:17:54 +0300

    Added predict, fit and print_score methods

--- start.py
+++ start.py
@@ -1,6 +1,11 @@
+import datetime
+
+import numpy as np
 import matplotlib.pyplot as plt
-from parser import parse_all
 from sklearn.cross_validation import KFold
+from sklearn.neighbors import KNeighborsRegressor
+
+from parser import parse_all
 
 
 class DataHandler(object):
@@ -8,17 +13,24 @@ class DataHandler(object):
     Handle the diabetes data set
     """
     def __init__(self):
+        self.times = []
+        self.labels = None
+        self.features = None
         self.data_list = parse_all()
         self.user = self.data_list[0]['data']
         self.activities = []
         self.dates = []
         self.glucose = []
         self.k_fold = []
+        self.classifier = KNeighborsRegressor()
 
     def parse_data(self):
         """
         Parse the data_list into our features
         """
+        train_data = []
+        test_data = []
+
         for user in self.data_list:
             for data in user['data']:
                 # I wan't to compress all the dataset in a 24 hours timeframe
@@ -28,12 +40,36 @@ class DataHandler(object):
                 self.dates.append(time)
                 self.activities.append(data['activity'])
                 self.glucose.append(data['glucose'])
+                train_data.append([int(time.strftime("%H")), int(time.strftime("%M")), data['activity']])
+                test_data.append([float(data['glucose'])])
+
+        self.features = np.array(train_data)
+        self.labels = np.array(test_data)
 
     def load_k_fold(self):
         """
         Load the test cases into K folds
         """
         self.k_fold = KFold(len(self.data_list), n_folds=len(self.data_list)*0.2)
+
+    def fit(self):
+        """
+        Train the classifier with the train data
+        """
+        self.classifier.fit(self.features, self.labels)
+
+    def print_score(self):
+        """
+        calculates the score on the given kfold test cases
+        """
+        for train, test in self.k_fold:
+            print(self.classifier.score(self.features[test], self.labels[test]))
+
+    def predict(self, date, activity):
+        """
+        Predict what will be the glucose rate on a given activity and data
+        """
+        return self.classifier.predict([int(date.strftime('%H')), int(date.strftime('%M')), activity])
 
     def plot(self):
         """
@@ -47,4 +83,8 @@ if __name__ == '__main__':
     dh = DataHandler()
     dh.parse_data()
     dh.load_k_fold()
+    dh.fit()
+    print(dh.predict(datetime.datetime.now(), 60))
+    dh.print_score()
     dh.plot()
+
